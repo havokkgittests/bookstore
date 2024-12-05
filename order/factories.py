df8540a8351c5a7fea50be9cd8333025269c5b16
havokkgittests <havokkmorandsforgames@gmail.com>
2024-12-05 14:28:33 -0300

    Add files via upload

---
+++ order/factories.py
@@ -0,0 +1,30 @@
+import factory
+
+from django.contrib.auth.models import User
+
+from order.models import Order
+from product.factories import ProductFactory
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    username = factory.Faker('user_name')
+    email = factory.Faker('email')
+
+
+    class Meta:
+        model = User
+
+class OrderFactory(factory.django.DjangoModelFactory):
+    user = factory.SubFactory(UserFactory)
+
+    @factory.post_generation
+    def product(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for product in extracted:
+                self.product.add(product)
+
+    class Meta:
+        model = Order
